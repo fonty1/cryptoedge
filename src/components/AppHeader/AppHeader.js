533ce86c6f574c397c04b0ce64034be99092e829
Matthew Fontana <mr.matto@gmail.com>
2017-05-08 10:24:43 +0300

    New sexy logo, conditional rendering improve
    + header fixes

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import './AppHeader.css';
-import cryptoedge from '../../img/cryptoedge4.png';
+import cryptoedge from '../../img/cryptoedge5.jpg';
 
 class AppHeader extends Component {
   render() {
     return <header>
-              <div className="logo-container">
-                <img src={cryptoedge} className="edge-logo"/>
-                <h1>CryptoEdge</h1>
-              </div>
+              <nav>
+                <ul className="primary-nav">
+                  <li className="logo-container left">
+                    <img src={cryptoedge} className="edge-logo"/>
+                    <h1>ryptoEdge</h1>
+                  </li>
+                  <li className="right">
+                    <i className="fa fa-refresh" aria-hidden="true"></i>
+                    Refresh
+                  </li>
+                  <li className="right">
+                    <i className="fa fa-question" aria-hidden="true"></i>
+                    Help
+                  </li>
+                  <li className="right">
+                    <i className="fa fa-envelope-o" aria-hidden="true"></i>
+                    Feedback
+                  </li>
+                </ul>
+              </nav>
+
            </header>
   }
 }
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,36 +6,44 @@ import renderIf  from 'render-if';
 
 class Portfolio extends Component {
     render() {
-      return (
-        renderIf(this.props.portfolioCryptoList.length > 0)(
-            <div className="Portfolio">
-                <Table responsive striped className="cryptotable">
-                        <thead>
-                           <tr>
-                              <th></th>
-                              <th>Cap</th>
-                              <th className="cryptoid">Crypto</th>
-                              <th className="holding">Coin #</th>
-                              <th className="holding">Total USD</th>
-                              <th className="holding">%</th>
-                              <th>USD</th>
-                              <th>BTC</th>
-                              <th>1 Hr</th>
-                              <th>24 Hrs</th>
-                              <th>7 Days</th>
-                           </tr>
-                         </thead>
+        const portfolioFilled = this.props.portfolioCryptoList.length > 0;
 
-                 <tbody>
-                     {this.props.portfolioCryptoList.map(function(crypto, index){
-                       return (
-                           <PortCoin crypto={crypto} index={index} key={index} />
-                       ) }, this )}
-                 </tbody>
-                </Table>
-            </div>
-        )
-        )
+         if (portfolioFilled) {
+           return (
+               <div className="Portfolio">
+                   <Table responsive striped className="cryptotable">
+                           <thead>
+                              <tr>
+                                 <th></th>
+                                 <th>Cap</th>
+                                 <th className="cryptoid">Crypto</th>
+                                 <th className="holding">Coin #</th>
+                                 <th className="holding">Total USD</th>
+                                 <th className="holding">%</th>
+                                 <th>USD</th>
+                                 <th>BTC</th>
+                                 <th>1 Hr</th>
+                                 <th>24 Hrs</th>
+                                 <th>7 Days</th>
+                              </tr>
+                            </thead>
+
+                    <tbody>
+                        {this.props.portfolioCryptoList.map(function(crypto, index){
+                          return (
+                              <PortCoin crypto={crypto} index={index} key={index} />
+                          ) }, this )}
+                    </tbody>
+                   </Table>
+               </div>
+           );
+           } else {
+             return (
+                 <div className="Portfolio">
+                    boot
+                 </div>
+             );
+            }
     }
 }
 
